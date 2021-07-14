189fdbbe10a41520a1dcec89df096182f6efc4a8
Ed Gomolyako <eduard.gomolyako@gmail.com>
2021-07-14 09:00:11 +0200

    Remove TELEGRAM_CHAT_ID variable from the call-for-rsvp script relying on the so-called environment variable

--- call-for-rsvp.sh
+++ call-for-rsvp.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-if [ -z "$TELEGRAM_BOT_TOKEN" ]; then
-  echo "Could n't send a notification for the call for RSVP. Telegram token isn't configured." >&2
+if [ -z "$TELEGRAM_BOT_TOKEN" -o -z "$TELEGRAM_CHAT_ID" ]; then
+  echo "Could n't send a notification for the call for RSVP. Telegram token or/and char aren't configured." >&2
   exit 0
 fi
-
-TELEGRAM_CHAT_ID="-488532756"
 
 function send_message
 {
