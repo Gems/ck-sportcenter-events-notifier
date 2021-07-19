b26736b0eaac113aba9f34453c5e541a2b2b34ec
Ed Gomolyako <eduard.gomolyako@gmail.com>
2021-07-19 13:34:44 +0200

    Fix the call-for-rsvp.sh script

--- call-for-rsvp.sh
+++ call-for-rsvp.sh
@@ -7,9 +7,7 @@ fi
 
 function send_message
 {
-  curl -s "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
-       -H "Content-Type: application/json" \ 
-       -d "{'chat_id': $TELEGRAM_CHAT_ID, 'text': '$1'}" 
+  curl -s "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -H "Content-Type: application/json" -d "{'chat_id': $TELEGRAM_CHAT_ID, 'text': '$1'}" 
 }
 
 
